d04672f2fb5953d44093c8d343d46a2eeddac8bb
iopdude <aidangd123@gmail.com>
2019-06-13 13:22:53 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+let http = require('http').Server(app);
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname+'/index.html'));
+});
+
+http.listen(PORT, (err) => {
+    if (err) {
+        return console.log('Problem starting server', err);
+    }
+
+    console.log(`Listening on ${PORT}`);
+});
